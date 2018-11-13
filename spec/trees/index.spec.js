fbeac335ff2790297bf55e8bd2c42823d0b4880a
Dzheni Dilcheva <ddilcheva@newclassrooms.org>
2018-11-13 09:55:03 -0500

    Add hasPathWithGivenSum

--- spec/trees/index.spec.js
+++ spec/trees/index.spec.js
@@ -3,7 +3,7 @@ import mocha from 'mocha';
 import chai from 'chai';
 import {  } from '../../lib/trees/has-path-with-given-sum';
 import { MyBinaryTree } from '../../lib/trees/tree';
-import { findAllSums } from '../../lib/trees/has-path-with-given-sum.mjs';
+import { findAllSums, hasPathWithGivenSum } from '../../lib/trees/has-path-with-given-sum.mjs';
 
 const {describe, it} = mocha;
 const {expect} = chai;
@@ -12,43 +12,21 @@ const {expect} = chai;
 describe('Binary Trees', function () {
 
 
-  describe('#populate', function () {
+  describe('#generateCompleteTree', function () {
 
 
     it('should handle a single node', function () {
-      const actual = new MyBinaryTree().populate([1]).root;
+      const actual = new MyBinaryTree().generateCompleteTree([1]).root;
       expect(actual).to.deep.equal({value: 1, left: null, right: null});
     });
 
-
-    it('should handle uneven number of nodes', function () {
-      const actual = new MyBinaryTree().populate([1,2,3,4,5]).root;
-      const expected = {
-        value: 1,
-        left: {
-          value: 2,
-          left: {
-            value: 4,
-            left: null,
-            right: null
-          },
-          right: null
-        },
-        right: {
-          value: 3,
-          left: {
-            value: 5,
-            left: null,
-            right: null
-          },
-          right: null
-        }
-      };
-      expect(actual).to.deep.equal(expected);
+    it('completes a level before starting a new one', function () {
+      const actual = new MyBinaryTree().generateCompleteTree([1, 2, 3]).root;
+      expect(actual).to.deep.equal({value: 1, left: { value: 2, right: null, left: null}, right: { value: 3, right: null, left: null}});
     });
 
-    it('should handle even number of nodes', function () {
-      const actual = new MyBinaryTree().populate([1,2,3,4,5,6]).root;
+    it('adds children left to right', function () {
+      const actual = new MyBinaryTree().generateCompleteTree([1,2,3,4,5,6]).root;
       const expected = {
         value: 1,
         left: {
@@ -80,10 +58,25 @@ describe('Binary Trees', function () {
   });
 
   describe('#findAllSums', function () {
-    let tree = new MyBinaryTree().populate([1, 2, 3, 4, 5, 6]).root;
-    const actual = findAllSums(tree);
-    const expected = [7, 8, 10];
-    expect(actual).to.deep.equal(expected);
+    it('returns all sums starting with the leftmost leaf', function () {
+      let tree = new MyBinaryTree().generateCompleteTree([1, 2, 3, 4, 5, 6]).root;
+      const actual = findAllSums(tree);
+      const expected = [7, 8, 10];
+      expect(actual).to.deep.equal(expected);
+    });
+  });
+
+  describe('#hasPathWithGivenSum', function () {
+    it('finds sum', function () {
+      let tree = new MyBinaryTree().generateCompleteTree([1, 2, 3, 4, 5, 6]).root;
+      const actual = hasPathWithGivenSum(tree, 10);
+      expect(actual).to.equal(true);
+    });
+    it('returns false when there is no match', function () {
+      let tree = new MyBinaryTree().generateCompleteTree([1, 2, 3, 4, 5, 6]).root;
+      const actual = hasPathWithGivenSum(tree, 9);
+      expect(actual).to.equal(false);
+    });
   });
 
 });
